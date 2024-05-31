53373c84a6c6ad65e3e37d47b25942ff7d993dbc
Benjamin Altpeter <hi@bn.al>
2024-05-31 20:13:20 +0200

    create proceedings

---
+++ src/pages/p/[platform]/[appId]/index.ts
@@ -0,0 +1,33 @@
+import type { APIRoute } from 'astro';
+import { nanoid } from 'nanoid';
+import { z } from 'zod';
+import { client, e } from '../../../../lib/db';
+
+const createProceedingSchema = z.object({
+    platform: z.enum(['android', 'ios']),
+    appId: z.string().max(150),
+});
+
+export const POST: APIRoute = async ({ params, redirect }) => {
+    const { platform, appId } = createProceedingSchema.parse(params);
+
+    const token = nanoid();
+
+    await e
+        .insert(e.Proceeding, {
+            app: e
+                .insert(e.App, {
+                    platform,
+                    appId,
+                })
+                .unlessConflict((app) => ({
+                    on: e.tuple([app.platform, app.appId]),
+                    else: app,
+                })),
+
+            token,
+        })
+        .run(client);
+
+    return redirect(`/p/${token}`);
+};

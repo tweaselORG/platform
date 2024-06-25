dc372e783658f01dfe60a208830a1b907d28d22a
Benjamin Altpeter <hi@bn.al>
2024-06-25 15:14:17 +0200

    Run integration tests in CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+    projectId: 'aqhwyr',
+
     viewportWidth: 1920,
     viewportHeight: 1080,
+
+    retries: 3,
 
     e2e: {
         baseUrl: 'http://localhost:4321',
